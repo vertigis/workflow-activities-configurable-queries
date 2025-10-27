c1810bcfcb0a503ae05ccd6d750f7f5dfea4f994
colin.doak@vertigis.com <colin.doak@vertigis.com>
2025-10-27 12:34:21 -0700

    fix: fix falsey currentValue test.

--- src/activities/GenerateQueryWhereClause.ts
+++ src/activities/GenerateQueryWhereClause.ts
@@ -114,7 +114,7 @@ export default class GenerateWhereClause implements IActivityHandler {
         field: __esri.Field,
     ): string | undefined {
         const currentValue = getFormValue(formElement, queryField);
-        if (currentValue) {
+        if (currentValue !== undefined && currentValue !== null) {
             return this.formatValue(currentValue, queryField, field);
         }
         return undefined;
